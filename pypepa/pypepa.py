d3569d22ad55a1066591305d5757b6299c770128
Allan Clark <allan.clark@gmail.com>
2013-10-07 11:42:18 +0100

    Added support for logging and changed the current output to logging.info

--- pypepa/pypepa.py
+++ pypepa/pypepa.py
@@ -1,6 +1,8 @@
 """The main source of the pypepa library"""
 
 import argparse
+import logging
+
 import pyparsing
 from pyparsing import OneOrMore, Or
 
@@ -55,14 +57,37 @@ def parse_file(filename):
     with open(filename, "r") as pepa_file:
         model_string = pepa_file.read()
         model = parse_model(model_string)
-        # TODO: All of this should go to a log, not the stdout
-        print (model)
-        print ("Defined process names:")
+
+        logging.debug (model)
+        logging.info ("Defined process names:")
         for name in defined_process_names(model):
-            print ("    " + name)
-        print ("Referenced process names:")
+            logging.info ("    " + name)
+        logging.info ("Referenced process names:")
         for name in used_process_names(model):
-            print ("    " + name)
+            logging.info ("    " + name)
+
+def initialise_logger(arguments):
+  """Initialise the logging system, depending on the arguments
+     which may set the log level and a log file"""
+  log_level = arguments.loglevel
+  if log_level:
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+      print ("The log level must be one of the following:")
+      print ("    debug, info, warning, error, critical")
+      print ("Exiting")
+      sys.exit(1) 
+  else:
+    numeric_level = logging.INFO
+
+  # We could also change the format of the logging messages to
+  # something like: format='%(levelname)s:%(message)s'
+
+  log_file = arguments.logfile
+  if log_file:
+    logging.basicConfig(filename=log_file, level=numeric_level)
+  else:
+    logging.basicConfig(level=numeric_level)
 
 def run ():
     """perform the banalities of command-line argument processing 
@@ -78,8 +103,24 @@ def run ():
                         help="The command to perform: " + ", ".join(command_choices))
     parser.add_argument('filenames', metavar='F', nargs='+',
                         help="A PEPA file")
+    log_choices = [ "info", "warning", "error", "critical", "debug" ]
+    parser.add_argument('--loglevel', action='store',
+                        choices=log_choices, default='info',
+                        help="Set the level of the logger")
+    parser.add_argument('--logfile', action='store', default=None,
+                        help="The file to output the log to")
 
     arguments = parser.parse_args()
+
+    # Initialise the logger
+    numerical_level = getattr(logging, arguments.loglevel.upper(), None)
+    if arguments.logfile:
+        logging.basicConfig(filename=arguments.logfile, level=numerical_level)
+    else:
+        logging.basicConfig(level=numerical_level)
+    # We could also change the format of the logging messages to
+    # something like: format='%(levelname)s:%(message)s'
+
     for filename in arguments.filenames:
         if arguments.command == "parse":
             parse_file(filename)

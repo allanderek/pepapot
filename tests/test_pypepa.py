8626c75eee8ce1ec7cc13dd34237cfd9f8c21ab5
Allan Clark <allan.clark@gmail.com>
2013-10-08 12:28:46 +0100

    Added a test for used process names such that there are some non-used process names

--- tests/test_pypepa.py
+++ tests/test_pypepa.py
@@ -13,25 +13,31 @@ import logging
 
 from pypepa import pypepa
 
-simple_source = """
+simple_components = """
 P = (a,r).P1;
 P1 = (b, r).P;
 
 Q = (a,r).Q1;
 Q1 = (b, r).Q;
-
-P || Q
 """
+simple_no_coop = simple_components + "\nP||Q"
+
 
 class TestPypepa(unittest.TestCase):
     def test_used_names(self):
-        model = pypepa.parse_model(simple_source)
+        model = pypepa.parse_model(simple_no_coop)
         used_names = model.used_process_names()
         expected = set(["P", "P1", "Q", "Q1"])
         self.assertEqual(used_names, expected)
+
+        simple_no_q = simple_components + "\nP"
+        model = pypepa.parse_model(simple_no_q)
+        used_names = model.used_process_names()
+        expected = set(["P", "P1"])
+        self.assertEqual(used_names, expected)
         
     def test_defined_names(self):
-        model = pypepa.parse_model(simple_source)
+        model = pypepa.parse_model(simple_no_coop)
         defined_names = pypepa.defined_process_names(model)
         expected = set(["P", "P1", "Q", "Q1"])
         self.assertEqual(defined_names, expected)

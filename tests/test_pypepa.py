7e511b407109112a4e7a3e8916f2165afefb61d5
adc <allan.clark@gmail.com>
2013-10-12 16:02:16 +0100

    Rearranged the test cases so now we have a class which tests the simple base model and all we have to do for each test model is to inherit from this base test class and override setUp with the model source and the expected processes, actions etc

--- tests/test_pypepa.py
+++ tests/test_pypepa.py
@@ -20,16 +20,11 @@ P1 = (b, r).P;
 Q = (a,r).Q1;
 Q1 = (b, r).Q;
 """
-simple_no_coop = simple_components + "\nP || Q"
-simple_single_coop = simple_components + "\nP < a > Q"
-simple_double_coop = simple_components + "\nP <a,b> Q"
 
-choice_component = """
+simple_choice_component = """
 P = (a, r).P1 + (b, r).P2;
 P1 = (c, r).P;
 P2 = (d, r).P;
-
-P
 """
 
 def create_expected_action_test(testcase, model, expected_actions):
@@ -42,55 +37,83 @@ def create_successors_test(testcase, model, expected_successors):
     actual_successors = model.get_successors()
     testcase.assertEqual(actual_successors, expected_successors)
 
-class TestPypepa(unittest.TestCase):
-    def test_used_names(self):
-        model = pypepa.parse_model(simple_no_coop)
-        used_names = model.used_process_names()
-        expected = set(["P", "P1", "Q", "Q1"])
-        self.assertEqual(used_names, expected)
+class TestModelBase(unittest.TestCase):
+    """A simple couple of definitions with a system equation involving no
+       cooperation. This also acts as a good base class to inherit from for
+       testing a model. One needs only to override the model definition and
+       the expected answers.
+    """
+    def setUp(self):
+        self.model_source = simple_components + "\nP || Q"
+        # It would be nice to actually test the parsing but that doesn't
+        # seem to be possible.
+        self.model = pypepa.parse_model(self.model_source)
+        self.expected_used_process_names = set(["P", "P1", "Q", "Q1"])
+        self.expected_defined_process_names = set(["P", "P1", "Q", "Q1"])
+    
+        self.expected_actions_dictionary = dict()
+        self.expected_actions_dictionary["P"] = [ "a" ]
+        self.expected_actions_dictionary["P1" ] = [ "b" ]
+        self.expected_actions_dictionary["Q" ] = [ "a" ]
+        self.expected_actions_dictionary["Q1" ] = [ "b" ]
+    
+        self.expected_successors_dictionary = dict()
+        self.expected_successors_dictionary["P"] = [ "P1" ]
+        self.expected_successors_dictionary["P1"] = [ "P" ]
+        self.expected_successors_dictionary["Q"] = [ "Q1" ]
+        self.expected_successors_dictionary["Q1"] = [ "Q" ]
 
-        simple_no_q = simple_components + "\nP"
-        model = pypepa.parse_model(simple_no_q)
-        used_names = model.used_process_names()
-        expected = set(["P", "P1"])
-        self.assertEqual(used_names, expected)
+    def test_used_names(self):
+        used_names = self.model.used_process_names()
+        self.assertEqual(used_names, self.expected_used_process_names)
 
     def test_defined_names(self):
-        model = pypepa.parse_model(simple_no_coop)
-        defined_names = pypepa.defined_process_names(model)
-        expected = set(["P", "P1", "Q", "Q1"])
-        self.assertEqual(defined_names, expected)
+        defined_names = pypepa.defined_process_names(self.model)
+        self.assertEqual(defined_names, self.expected_defined_process_names)
 
-    def test_cooperation_parser(self):
-        model = pypepa.parse_model(simple_single_coop)
-        self.assertEqual(model.system_equation.cooperation_set, ["a"])
-        model = pypepa.parse_model(simple_no_coop)
-        self.assertEqual(model.system_equation.cooperation_set, [])
-        model = pypepa.parse_model(simple_double_coop)
-        self.assertEqual(model.system_equation.cooperation_set, ["a", "b"])
+    # TODO: Somehow test the correct parsing of coopertions (system equation)
 
     def test_actions(self):
-        actions = dict()
-        actions["P"] = [ "a" ]
-        actions["P1" ] = [ "b" ]
-        actions["Q" ] = [ "a" ]
-        actions["Q1" ] = [ "b" ]
-        create_expected_action_test(self, simple_single_coop, actions)
-
-    def test_choice(self):
-        actions = dict()
-        actions["P"] = [ "a", "b" ]
-        actions["P1" ] = [ "c" ]
-        actions["P2" ] = [ "d" ]
-        create_expected_action_test(self, choice_component, actions)
+        actual_actions = self.model.get_process_actions()
+        self.assertEqual(actual_actions, self.expected_actions_dictionary)
 
     def test_successors(self):
-        successors = dict()
-        successors["P"] = [ "P1" ]
-        successors["P1"] = [ "P" ]
-        successors["Q"] = [ "Q1" ]
-        successors["Q1"] = [ "Q" ]
-        create_successors_test(self, simple_no_coop, successors)
+        actual_successors = self.model.get_successors()
+        self.assertEqual(actual_successors, self.expected_successors_dictionary)
+
+class TestSimpleSingleCoop(TestModelBase):
+    def setUp(self):
+        # This model has most of the same results as the base test case
+        # So I'll just call super here rather than copy and paste, but in
+        # general it shouldn't be necessary to call super here.
+        super(TestSimpleSingleCoop, self).setUp()
+        self.model_source = simple_components + "\nP <a> Q"
+        self.model = pypepa.parse_model(self.model_source)
+
+class TestSimpleDoubleCoop(TestModelBase):
+    def setUp(self):
+        # Similar to the above case we're only using super here because we can
+        # and so many of th expected results are the same.
+        super(TestSimpleDoubleCoop, self).setUp()
+        self.model_source = simple_components + "\nP <a, b> Q"
+        self.model = pypepa.parse_model(self.model_source)
+
+
+class TestPypepa(unittest.TestCase):
+    """A simple test only because I'm not sure how to generically test the
+       parsing of the system equation. Once I figure that out I can move this
+       into TestModelBase
+    """
+    def test_cooperation_parser(self):
+        model_source = simple_components + "\n P <a> P"
+        model = pypepa.parse_model(model_source)
+        self.assertEqual(model.system_equation.cooperation_set, ["a"])
+        model_source = simple_components + "\nP || Q"
+        model = pypepa.parse_model(model_source)
+        self.assertEqual(model.system_equation.cooperation_set, [])
+        model_source = simple_components + "\nP <a,b>Q"
+        model = pypepa.parse_model(model_source)
+        self.assertEqual(model.system_equation.cooperation_set, ["a", "b"])
 
 class ExpectedFailureTestCase(unittest.TestCase):
     @unittest.expectedFailure

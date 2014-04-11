21005ed00a92b02aecec84d53e42dd765c1a9b88
Allan Clark <allan.clark@gmail.com>
2014-04-11 10:16:47 +0100

    Removed a TODO comment because I had actually already done the thing in question

--- tests/test_pepapot.py
+++ tests/test_pepapot.py
@@ -56,7 +56,6 @@ class TestMissingNameExpression(TestExpression):
 
 class TestConstantDefinitions(unittest.TestCase):
     def setUp(self):
-        # TODO: Add exponentiation when that is implemented in the parser
         self.source = """a = 1.0;
                          b = a + 10.0;
                          c = b - 1.0;
